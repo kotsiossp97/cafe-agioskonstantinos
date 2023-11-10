f40e4de6766441b1921af14f039106328b93e5ac
kotsiossp97 <kotsiossp@gmail.com>
2023-11-10 10:36:01 +0200

    Add firebase analytics

--- src/App.js
+++ src/App.js
@@ -12,7 +12,27 @@ import { initReactI18next } from "react-i18next";
 import translationGR from "./locales/gr";
 import translationEN from "./locales/en";
 import menuData from "./data/menu";
-import LanguageDetector from 'i18next-browser-languagedetector';
+import LanguageDetector from "i18next-browser-languagedetector";
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+    apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+    authDomain: "cafe-agioskonstantinos.firebaseapp.com",
+    databaseURL: process.env.REACT_APP_FIREBASE_DB_URL,
+    projectId: "cafe-agioskonstantinos",
+    storageBucket: "cafe-agioskonstantinos.appspot.com",
+    messagingSenderId: process.env.REACT_APP_FIREBASE_MSG_SENDER_ID,
+    appId: process.env.REACT_APP_FIREBASE_APP_ID,
+    measurementId: process.env.REACT_APP_FIREBASE_MEAS_ID,
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+// eslint-disable-next-line no-unused-vars
+const analytics = getAnalytics(app);
 
 const resources = {
     "el-GR": {
@@ -23,28 +43,18 @@ const resources = {
     },
 };
 
-
-i18n
-.use(LanguageDetector)
-.use(initReactI18next).init({
-    resources,
-    fallbackLng: "en-US",
-    interpolation: {
-        escapeValue: false,
-    },
-})
+i18n.use(LanguageDetector)
+    .use(initReactI18next)
+    .init({
+        resources,
+        fallbackLng: "en-US",
+        interpolation: {
+            escapeValue: false,
+        },
+    });
 
 const App = () => {
-    // const [menuItems, setMenuItems] = useState(null);
-    // useEffect(() => {
-    //     fetch(
-    //         "https://cafe-agioskonstantinos-default-rtdb.europe-west1.firebasedatabase.app/.json"
-    //     )
-    //         .then((resp) => resp.json())
-    //         .then((data) => setMenuItems(data));
-    // }, []);
-
-    const menuItems = menuData
+    const menuItems = menuData;
 
     return (
         <ThemeProvider theme={theme}>
